d62bfdc32ef25dde00f5b644a671a1b3b7cd80cc
theQuazz <djrquazz@gmail.com>
2015-01-31 17:37:56 -0500

    Finish up fb stuff

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,10 +1,11 @@
-
+var _    = require('lodash');
 var User = require('../models/user');
 
 var users = {};
 
 users.create = function(req, res) {
-  res.status(req.user ? 200 : 401).json(req.user);
+  console.log(req.user);
+  res.status(req.user ? 200 : 401).json(_.omit(req.user.toObject(), 'fbToken', '__v'));
 }
 
 
