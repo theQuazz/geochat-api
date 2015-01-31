1d00129e095c1cd751c979545693404a6be7abf4
theQuazz <djrquazz@gmail.com>
2015-01-31 18:15:48 -0500

    Fix?

--- app/models/message.js
+++ app/models/message.js
@@ -4,12 +4,12 @@ var Schema   = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
 var Message = new Schema({
-  user:     { type: ObjectId, ref: 'User', required: true },
+  user:     { type: ObjectId, ref: 'User' }, //, required: true },
   message:  { type: String, required: true },
   loc:      { type: [Number], index: '2dsphere', required: true },
   sentAt:   { type: Date, default: Date.now, required: true },
   sendAt:   { type: Date, default: Date.now, required: true },
-  duration: { type: Number, default: Infinity, required: true },
+  duration: { type: Number, default: 1e9, required: true },
 });
 
 module.exports = mongoose.model('Message', Message);
